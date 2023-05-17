b9f6d38da10178638770561dc3bef121cfd7ffcc
Amandacamargo21 <amandacamargo2102@gmail.com>
2023-05-17 15:00:45 -0300

    ok

---
+++ client/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Card, CardFooter, } from "reactstrap";
+
+
+function Footer() {
+
+  
+    return (
+    <Card className='mt-5'>
+        <CardFooter className="text-muted text-center">
+            Company test - Shaw and Partners
+        </CardFooter>
+    </Card>
+    );
+  }
+  
+  export default Footer;
